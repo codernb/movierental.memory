8c193976ed252f25b3b3cf5b06ce7d4277d26624
codernb <cyril.halmo@gmx.ch>
2016-10-07 16:25:04 +0200

    More output.

--- src/test/java/ch/fhnw/edu/rental/persistence/jdbc/MovieRepositoryJdbcTest.java
+++ src/test/java/ch/fhnw/edu/rental/persistence/jdbc/MovieRepositoryJdbcTest.java
@@ -23,34 +23,57 @@ public class MovieRepositoryJdbcTest {
 
 	@Autowired
 	private MovieRepository movieRepository;
-	
+
 	@Autowired
 	private RentalRepository rentalRepository;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PriceCategoryRepository priceCategoryRepository;
-	
+
 	@After
-	public void tearDown() 
-	{
+	public void tearDown() {
+		System.out.println();
+		System.out.println("--");
+		System.out.println("tearDown");
+		System.out.println("mr.count: " + movieRepository.count());
+		System.out.println("mr.findAll.size: " + movieRepository.findAll().size());
+		System.out.println("---delete---");
+
 		rentalRepository.findAll().forEach(rentalRepository::delete);
 		movieRepository.findAll().forEach(movieRepository::delete);
 		userRepository.findAll().forEach(userRepository::delete);
-		System.err.println(movieRepository.count());
+
+		System.out.println("mr.count: " + movieRepository.count());
+		System.out.println("mr.findAll.size: " + movieRepository.findAll().size());
+		System.out.println("--");
+		System.out.println();
 	}
-	
+
 	@Test
 	public void testFindOne() throws Exception {
-		System.err.println(movieRepository.count());
+		System.err.println();
+		System.err.println("--");
+		System.err.println("testFindOne");
+		System.err.println("mr.count: " + movieRepository.count());
+		System.err.println("mr.findAll.size: " + movieRepository.findAll().size());
+		System.err.println("--");
+		System.err.println();
+		
 		movieRepository.save(new Movie("test", new Date(), priceCategoryRepository.findOne(1L)));
 	}
-	
+
 	@Test
 	public void testFindOne1() throws Exception {
-		System.err.println(movieRepository.count());
+		System.err.println();
+		System.err.println("--");
+		System.err.println("testFindOne1");
+		System.err.println("mr.count: " + movieRepository.count());
+		System.err.println("mr.findAll.size: " + movieRepository.findAll().size());
+		System.err.println("--");
+		System.err.println();
 	}
 
 }
